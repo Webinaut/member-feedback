c85fbab2ee8961b6e4143c42d717232df12a524a
Martin <slackgt@bigpond.com>
2025-10-11 14:31:47 +1100

    11102025-6

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,10 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
-import sitemap from '@astrojs/sitemap';
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [sitemap()]
+  site: "https://memberfeedback.netlify.app/", // Replace with your actual site URL
+  integrations: [sitemap()],
 });
